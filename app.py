d66e2912b03391bdfd97145bdaddf3657700887d
abhiramdas99 <abhiramdas99@gmail.com>
2022-12-17 23:51:59 +0530

    database connetion completed

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
 
+# step 1 - import library 
+from flask import Flask,render_template,request
+
+#step 2 - import database library
 from flask_mysqldb import MySQL
 
 app= Flask(__name__)
 
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']='python_web_mysql'
+
+mysql=MySQL(app)
+
+
 @app.route('/')
 def index():
-    return "hello"
+    cur=mysql.connection.cursor()
+    data=cur.execute("select * from employee_detail")
+    if data>0:
+        employees=cur.fetchall()
+    
+    return render_template('index.html',employees=employees)
 
 
 if __name__=="__main__":
